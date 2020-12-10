802ad0c2f924c4ff36efc8e9e2325764daeb3fbb
sihaloho apry <madonaapry@gmail.com>
2020-12-10 01:41:19 -0800

    SUBMISSION FINAL -(SIAP KUMPUL KE 2)

--- app/src/androidTest/java/com/sihaloho/catalogmovie/ui/MainActivityTest.kt
+++ app/src/androidTest/java/com/sihaloho/catalogmovie/ui/MainActivityTest.kt
@@ -1,8 +1,10 @@
 package com.sihaloho.catalogmovie.ui
 
 import androidx.recyclerview.widget.RecyclerView
+import androidx.test.core.app.ActivityScenario
 import androidx.test.espresso.Espresso
 import androidx.test.espresso.Espresso.openActionBarOverflowOrOptionsMenu
+import androidx.test.espresso.IdlingRegistry
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions
@@ -13,6 +15,9 @@ import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.rule.ActivityTestRule
 import com.sihaloho.catalogmovie.R
 import com.sihaloho.catalogmovie.ui.activity.MainActivity
+import com.sihaloho.catalogmovie.utils.EspressoIdlingResource
+import org.junit.After
+import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 
@@ -22,50 +27,50 @@ class MainActivityTest {
     @get:Rule
     var activityRule = ActivityTestRule(MainActivity::class.java)
 
+    @Before
+    fun setUp() {
+        ActivityScenario.launch(MainActivity::class.java)
+        IdlingRegistry.getInstance().register(EspressoIdlingResource.idlingResource)
+    }
+
+    @After
+    fun tearDown() {
+        IdlingRegistry.getInstance().unregister(EspressoIdlingResource.idlingResource)
+    }
+
     @Test
     fun loadMovie() {
 
         //movie
-        Thread.sleep(1000)
         Espresso.onView(withId(R.id.rv_movie))
             .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        Thread.sleep(2000)
         Espresso.onView(withId(R.id.rv_movie)).perform(
             RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
                 0,
                 click()
             )
         )
-        Thread.sleep(1000)
         Espresso.onView(withId(R.id.fab_love)).perform(click())
-        Thread.sleep(2000)
         Espresso.onView(withId(R.id.iv_arrow_left)).perform(click())
-        Thread.sleep(1000)
         Espresso.onView(withId(R.id.rv_movie)).perform(
             RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
                 3,
                 click()
             )
         )
-        Thread.sleep(1000)
         Espresso.onView(withId(R.id.fab_love)).perform(click())
-        Thread.sleep(2000)
         Espresso.onView(withId(R.id.iv_arrow_left)).perform(click())
-        Thread.sleep(1000)
 
         //tvshow
         Espresso.onView(withId(R.id.rv_tvShow))
             .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
-        Thread.sleep(2000)
         Espresso.onView(withId(R.id.rv_tvShow)).perform(
             RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
                 0,
                 click()
             )
         )
-        Thread.sleep(1000)
         Espresso.onView(withId(R.id.fab_love)).perform(click())
-        Thread.sleep(2000)
         Espresso.onView(withId(R.id.iv_arrow_left)).perform(click())
         Espresso.onView(withId(R.id.rv_tvShow)).perform(
             RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
@@ -73,28 +78,22 @@ class MainActivityTest {
                 click()
             )
         )
-        Thread.sleep(1000)
         Espresso.onView(withId(R.id.fab_love)).perform(click())
-        Thread.sleep(2000)
         Espresso.onView(withId(R.id.iv_arrow_left)).perform(click())
-        Thread.sleep(1000)
 
         //favorite movie
         openActionBarOverflowOrOptionsMenu(activityRule.getActivity());
         Espresso.onView(withText("Favorite Movie"))
             .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
         Espresso.onView(withText("Favorite Movie")).perform(click())
-        Thread.sleep(1000)
         Espresso.onView(withId(R.id.rv_movie_fav)).perform(
             RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
                 0,
                 click()
             )
         )
-        Thread.sleep(1000)
         Espresso.onView(ViewMatchers.isRoot()).perform(ViewActions.pressBack())
         Espresso.onView(ViewMatchers.isRoot()).perform(ViewActions.pressBack())
-        Thread.sleep(1000)
 
 
         //favorite tv show
@@ -104,59 +103,46 @@ class MainActivityTest {
             .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
         Espresso.onView(withText("Favorite Tv Show")).perform(click())
 
-        Thread.sleep(1000)
         Espresso.onView(withId(R.id.rv_tv_show_fav)).perform(
             RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
                 0,
                 click()
             )
         )
-        Thread.sleep(1000)
         Espresso.onView(ViewMatchers.isRoot()).perform(ViewActions.pressBack())
         Espresso.onView(ViewMatchers.isRoot()).perform(ViewActions.pressBack())
 
         //delete favorite
-        Thread.sleep(1000)
         Espresso.onView(withId(R.id.rv_movie)).perform(
             RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
                 0,
                 click()
             )
         )
-        Thread.sleep(1000)
         Espresso.onView(withId(R.id.fab_love)).perform(click())
-        Thread.sleep(1000)
         Espresso.onView(withId(R.id.iv_arrow_left)).perform(click())
 
-        Thread.sleep(1000)
         Espresso.onView(withId(R.id.rv_tvShow)).perform(
             RecyclerViewActions.actionOnItemAtPosition<RecyclerView.ViewHolder>(
                 0,
                 click()
             )
         )
-        Thread.sleep(1000)
         Espresso.onView(withId(R.id.fab_love)).perform(click())
-        Thread.sleep(1000)
         Espresso.onView(withId(R.id.iv_arrow_left)).perform(click())
 
         //check favorite
-        Thread.sleep(1000)
         openActionBarOverflowOrOptionsMenu(activityRule.getActivity());
         Espresso.onView(withText("Favorite Tv Show"))
             .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
         Espresso.onView(withText("Favorite Tv Show")).perform(click())
-        Thread.sleep(1000)
         Espresso.onView(ViewMatchers.isRoot()).perform(ViewActions.pressBack())
-        Thread.sleep(1000)
 
         openActionBarOverflowOrOptionsMenu(activityRule.getActivity());
         Espresso.onView(withText("Favorite Tv Show"))
             .check(ViewAssertions.matches(ViewMatchers.isDisplayed()))
         Espresso.onView(withText("Favorite Tv Show")).perform(click())
-        Thread.sleep(1000)
         Espresso.onView(ViewMatchers.isRoot()).perform(ViewActions.pressBack())
-        Thread.sleep(1000)
 
     }
 }
--- app/src/main/java/com/sihaloho/catalogmovie/data/RemoteDataSource.kt
+++ app/src/main/java/com/sihaloho/catalogmovie/data/RemoteDataSource.kt
@@ -1,10 +1,12 @@
 package com.sihaloho.catalogmovie.data
 
+import android.os.Handler
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import com.sihaloho.catalogmovie.data.entity.MovieEntity
 import com.sihaloho.catalogmovie.data.entity.TvShowEntity
 import com.sihaloho.catalogmovie.data.retrofit.ApiConfig
+import com.sihaloho.catalogmovie.utils.EspressoIdlingResource
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.launch
@@ -12,7 +14,13 @@ import retrofit2.await
 import java.io.IOException
 
 class RemoteDataSource  {
+
+    private val handler = Handler()
+
+
     companion object {
+        private const val SERVICE_LATENCY_IN_MILLIS: Long = 2000
+
         @Volatile
         private var instance: RemoteDataSource? = null
 
@@ -23,43 +31,56 @@ class RemoteDataSource  {
     }
 
     fun getMovies() : LiveData<ApiResponse<List<MovieEntity>>> {
+        EspressoIdlingResource.increment()
         val resultMovie = MutableLiveData<ApiResponse<List<MovieEntity>>>()
+        handler.postDelayed({
 
-        CoroutineScope(IO).launch {
-            try {
-                val response = ApiConfig.instance.getMovies().await()
-                resultMovie.postValue(ApiResponse.success(response.results))
-            } catch (e: IOException) {
-                e.printStackTrace()
-                resultMovie.postValue(
-                    ApiResponse.error(
-                        e.message.toString(),
-                        mutableListOf()
+            CoroutineScope(IO).launch {
+                try {
+                    val response = ApiConfig.instance.getMovies().await()
+                    resultMovie.postValue(ApiResponse.success(response.results))
+                    EspressoIdlingResource.decrement()
+
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                    resultMovie.postValue(
+                        ApiResponse.error(
+                            e.message.toString(),
+                            mutableListOf()
+                        )
                     )
-                )
+                }
             }
-        }
+
+
+        }, SERVICE_LATENCY_IN_MILLIS)
         return resultMovie
 
 
     }
     fun getTvShow() : LiveData<ApiResponse<List<TvShowEntity>>> {
+        EspressoIdlingResource.increment()
         val resultTvShow = MutableLiveData<ApiResponse<List<TvShowEntity>>>()
 
-        CoroutineScope(IO).launch {
-            try {
-                val response = ApiConfig.instance.getTvShow().await()
-                resultTvShow.postValue(ApiResponse.success(response.results))
-            } catch (e: IOException) {
-                e.printStackTrace()
-                resultTvShow.postValue(
-                    ApiResponse.error(
-                        e.message.toString(),
-                        mutableListOf()
+        handler.postDelayed({
+            CoroutineScope(IO).launch {
+                try {
+                    val response = ApiConfig.instance.getTvShow().await()
+                    resultTvShow.postValue(ApiResponse.success(response.results))
+                    EspressoIdlingResource.decrement()
+
+                } catch (e: IOException) {
+                    e.printStackTrace()
+                    resultTvShow.postValue(
+                        ApiResponse.error(
+                            e.message.toString(),
+                            mutableListOf()
+                        )
                     )
-                )
+                }
             }
-        }
+
+        }, SERVICE_LATENCY_IN_MILLIS)
         return resultTvShow
 
 
